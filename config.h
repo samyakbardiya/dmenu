1dbae6bb14ee620039dc4e516f35c8f7660af2df
Samyak Bardiya <samyak039@gmail.com>
2022-03-19 23:48:14 +0530

    gruvbox-material

--- config.h
+++ config.h
@@ -12,24 +12,25 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *dynamic     = NULL;      /* -dy option; dynamic command to run on input change */
-// GRUVBOX_DARK
+ // GRUVBOX_MATERIAL
 static const char *colors[SchemeLast][2] = {
-                        	/*     fg        bg     */
-	         [SchemeNorm] = { "#ebdbb2", "#1d2021" },
-	          [SchemeSel] = { "#1d2021", "#cc241d" },
-	 [SchemeSelHighlight] = { "#1d2021", "#d79921" },
-	[SchemeNormHighlight] = { "#1d2021", "#d6540e" },
-	          [SchemeOut] = { "#1d2021", "#98971a" },
+                            /*     fg        bg     */
+             [SchemeNorm] = { "#D4BE98", "#202020" },
+              [SchemeSel] = { "#202020", "#EA6962" },
+     [SchemeSelHighlight] = { "#202020", "#D8A657" },
+    [SchemeNormHighlight] = { "#202020", "#E78A4E" },
+              [SchemeOut] = { "#202020", "#A9B665" },
 };
-//DRACULA
+// // GRUVBOX_DARK
 //static const char *colors[SchemeLast][2] = {
-//                        	/*     fg        bg     */
-//	         [SchemeNorm] = { "#f8f8f2", "#282a36" },
-//	          [SchemeSel] = { "#282a36", "#ff5555" },
-//	 [SchemeSelHighlight] = { "#282a36", "#f1fa8c" },
-//	[SchemeNormHighlight] = { "#f1fa8c", "#6272a4" },
-//	          [SchemeOut] = { "#000000", "#00ffff" },
+//                          /*     fg        bg     */
+//           [SchemeNorm] = { "#ebdbb2", "#1d2021" },
+//            [SchemeSel] = { "#1d2021", "#cc241d" },
+//   [SchemeSelHighlight] = { "#1d2021", "#d79921" },
+//  [SchemeNormHighlight] = { "#1d2021", "#d6540e" },
+//            [SchemeOut] = { "#1d2021", "#98971a" },
 //};
+
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
 static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
