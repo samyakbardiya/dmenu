8922e7702449ecdb776f972453b81ca715bac3a4
Samyak Bardiya <samyak039@gmail.com>
2022-05-18 19:14:34 +0530

    2021-05-18

--- config.def.h
+++ config.def.h
@@ -7,8 +7,8 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Fira code Nerd Font:size=12",
-	"JoyPixels:pixelsize=14:antialias=true:autohint=true"
+	"Fira Code Nerd Font:size=12",
+	"Twemoji:pixelsize=14:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *dynamic     = NULL;      /* -dy option; dynamic command to run on input change */
